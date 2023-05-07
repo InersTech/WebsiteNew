a63e688cc5c348e123d499e17311aaaa3fcb0975
anilmhrzn <anilmaharjan995@gmail.com>
2023-05-07 17:39:49 +0545

    added web file maintanance in progress

---
+++ README.md
@@ -0,0 +1,6 @@
+# website
+company website</br>
+Setup process
+1. Download the zip file and extract to xampp/htdocs</br>
+2. Create the database inserstech and import the file inserstech.sql file</br>
+3. Start xampp server and hit localhost/iners on browser
---
+++ action/application_process.php
@@ -0,0 +1,44 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+if($_POST['submit']){
+    include '../admin/includes/connection.php';
+
+if ($_FILES) {
+    $filename1 = $_FILES["c_letter"]["name"];
+    $tempname1 = $_FILES["c_letter"]["tmp_name"];
+    $folder1 = "../assets/files/cover/" . $filename1;
+    echo move_uploaded_file($tempname1, $folder1);
+    // exit;
+    // echo $folder1;
+    // exit;
+    // $folder1 = "../assets/files/cover/" . $filename1;
+    
+
+    $filename2 = $_FILES["resume"]["name"];
+    $tempname2 = $_FILES["resume"]["tmp_name"];
+    $folder2 = "../assets/files/resume/" . $filename2;
+    move_uploaded_file($tempname2, $folder2);
+ 
+}
+$post = $_POST['post'];
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+
+$sql = "INSERT INTO application(name,email,contact,post,cover,resume) values('$name','$email','$phone','$post','".$filename1."','".$filename2."')";
+$query= mysqli_query($connection,$sql);
+if($query){
+    echo "<script>
+    window.alert('Your application is submitted')
+    window.location.href='../index.php'
+    </script>";
+}
+else{
+    echo $connection->error;
+}
+}
+else{
+    echo "not called";
+}
